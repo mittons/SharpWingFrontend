03b38c9bac1734ccd4bdb413ddb5a08bcc471945
mittons <icyaxel@gmail.com>
2023-12-06 21:50:47 +0000

    Update config.dart

--- lib/config/config.dart
+++ lib/config/config.dart
@@ -17,7 +17,7 @@ class TestConfig extends AppConfig {
   static const String baseApiUrlValue = 'http://localhost:3000';
   final int containerPort = 5000;
   final int hostPort = 3000;
-  final String dockerImage = "ghcr.io/mittons/dockreg26:1.0";
+  final String dockerImage = "ghcr.io/mittons/sharpwingback:latest";
 
   TestConfig() : super(baseApiUrl: baseApiUrlValue);
 }
